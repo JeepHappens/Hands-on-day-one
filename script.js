2a20422cf74e31f36ed1e2d8f5aa2a510b2f9470
JeepHappens <robert.osterman12@gmail.com>
2017-06-05 11:06:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var gradeArray = [document.getElementById(g1), document.getElementById(g2), document.getElementById(g3), document.getElementById(g4), document.getElementById(g5), document.getElementById(g6), document.getElementById(g7), document.getElementById(g8), document.getElementById(g9), document.getElementById(g10)]
+
+
+
+function grades (){
+    for (var i=0; i<=gradeArray.length; i++)
+        if(i)
+}
