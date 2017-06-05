72157c6c4b43c0a48f3ebdd93190fabce994e172
JeepHappens <robert.osterman12@gmail.com>
2017-06-05 11:23:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-var gradeArray = [document.getElementById(g1), document.getElementById(g2), document.getElementById(g3), document.getElementById(g4), document.getElementById(g5), document.getElementById(g6), document.getElementById(g7), document.getElementById(g8), document.getElementById(g9), document.getElementById(g10)]
-
+var gradeArray = [document.getElementById(g1), document.getElementById(g2), document.getElementById(g3), document.getElementById(g4),
+                  document.getElementById(g5), document.getElementById(g6), document.getElementById(g7), document.getElementById(g8), 
+                  document.getElementById(g9), document.getElementById(g10)]
 
 
 function grades (){
     for (var i=0; i<=gradeArray.length; i++)
-        if(i)
+        if(gradeArray[i]>=0 && <=59){
+            return("F");
+        }
+        else if(gradeArray[i]>=60 && <=69){
+            return("D");
+        }
+        else if(gradeArray[i]>=70 && <=79){
+            return("C");
+        }
+        else if(gradeArray[i]>=80 && <=89){
+            return("B");
+        }
+        else if(gradeArray[i]>=90 && <=100){
+            return("A");
+        }
+        else{
+            return "Invalid Input"
+        }
 }
